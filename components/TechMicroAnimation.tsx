22c22862c346a1963ffe13eabd2713e2824664c2
vamshivytla1aws-ux <vamshivytla1aws@gmail.com>
2026-08-11 12:55:05 +0530

    feat: refine header character interaction

--- components/TechMicroAnimation.tsx
+++ components/TechMicroAnimation.tsx
@@ -12,26 +12,22 @@ export function TechMicroAnimation() {
       <path className="developer-laptop-base" d="M4 45h66l-5 6H9l-5-6Z" />
       <path className="developer-laptop-detail" d="M31 47h12" />
     </svg>
-    <svg className="developer-scene" viewBox="0 0 360 64" focusable="false">
-      <g className="developer-travel">
-        <g className="office-chair">
-          <rect className="chair-back" x="298" y="24" width="8" height="23" rx="3" />
-          <path className="chair-frame" d="M302 45h18m-8 0v10m-10 3h20" />
-          <circle className="chair-wheel" cx="302" cy="59" r="2" />
-          <circle className="chair-wheel" cx="322" cy="59" r="2" />
-        </g>
-        <g className="developer-body">
-          <circle className="developer-head" cx="313" cy="18" r="6" />
-          <path className="developer-hair" d="M307 18c0-7 11-9 13-2-4-2-8-2-13 2Z" />
-          <path className="developer-torso" d="M309 26c5-2 11-1 14 2l1 17h-18l1-15c0-2 1-3 2-4Z" />
-          <path className="developer-arm developer-arm-front" d="M310 29l-8 10 9 3" />
-          <path className="developer-arm developer-arm-back" d="M321 30l7 10" />
-          <g className="developer-legs">
-            <path className="developer-leg developer-leg-front" d="M311 44l12 6-2 10" />
-            <path className="developer-leg developer-leg-back" d="M318 44l-5 14" />
-          </g>
-          <path className="developer-shoe" d="M318 60h7m-15-1h7" />
-        </g>
+    <svg className="developer-character" viewBox="0 0 82 64" focusable="false">
+      <g className="developer-body">
+        <circle className="developer-head" cx="36" cy="14" r="7" />
+        <path className="developer-hair" d="M29 14c0-8 13-11 16-2-5-2-10-2-16 2Z" />
+        <g className="developer-front-features"><circle cx="33.5" cy="14.5" r=".75" /><circle cx="39" cy="14.5" r=".75" /><path d="M34 18c2 1 4 1 5 0" /></g>
+        <path className="developer-profile-feature" d="M29 15l-3 2.5 3 1" />
+        <path className="developer-torso" d="M30 23c5-2 12-1 15 3l1 19H27l1-16c0-3 1-5 2-6Z" />
+        <path className="developer-arm developer-home-arm" d="M43 27c8-1 15-5 21-7l11 1" />
+        <circle className="developer-home-hand" cx="77" cy="21" r="2" />
+        <path className="developer-arm developer-laptop-arm" d="M30 28c-8 1-14 5-21 8l-6-1" />
+        <circle className="developer-laptop-hand" cx="2" cy="35" r="2" />
+        <path className="developer-arm developer-walk-arm-front" d="M30 27l-8 14" />
+        <path className="developer-arm developer-walk-arm-back" d="M43 28l8 13" />
+        <path className="developer-leg developer-leg-front" d="M32 44l-3 16" />
+        <path className="developer-leg developer-leg-back" d="M41 44l4 16" />
+        <path className="developer-shoe" d="M25 61h7m11 0h7" />
       </g>
     </svg>
   </span>;
